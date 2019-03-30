9fc3987b4c5a1a305327eec6f3e38ff2b4a66839
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-30 22:53:01 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,44 @@
-# Write your code here.
+def dictionary
+  dict = {
+    "hello" => 'hi',
+    "to" => '2', 
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+
+end
+
+def word_substituter(tweet)
+  
+  dict = dictionary 
+  words = tweet.split(" ")
+  
+  words.map do |word| 
+   dict.map do |key,value|
+    if word.downcase == key
+     word.replace(value)
+    end
+   end
+  end
+ 
+   words.join(" ")
+end
+  
+  
+def bulk_tweet_shortener(tweets_array)
+  
+   tweets_array.map do |tweet| 
+    puts word_substituter(tweet)
+   end
+
+end
+  
+  
+  
+  
